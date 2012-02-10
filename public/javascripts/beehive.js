d8adc0747170aa15c7a955ef3523b42a83076733
Attila Györffy <attila@attilagyorffy.com>
2012-02-10 00:30:54 +0000

    Make Beehive conform with CommonJS so that it can be used within Node and in the browser too.

--- public/javascripts/beehive.js
+++ public/javascripts/beehive.js
@@ -1,24 +1,24 @@
-var Beehive = new function() {
-  this.Behaviours = {};
-  
-  this.make = function make(target) {
+(function(exports) {
+  exports.Behaviours = {};
+  exports.make = function make(target) {
+    
     function behave_like_a(behaviour_type, params) {
-      var behaviour_func = Beehive.Behaviours[constantize(behaviour_type)];
+      var behaviour_func = exports.Behaviours[constantize(behaviour_type)];
       behaviour_func.call(target, params);
     };
+    
     return {
       behave_like_a: behave_like_a,
       behave_like_an: behave_like_a
     };
   };
-};
+  
+  function constantize(string) {
+    var words = string.split(/[\W_]/);
+    for (var i=0; i<words.length; i++) {
+      words[i] = words[i].substr(0, 1).toUpperCase() + words[i].substr(1);
+    }
+    return words.join('');
+  };
 
-
-
-function constantize(string) {
-  var words = string.split(/[\W_]/);
-  for (var i=0; i<words.length; i++) {
-    words[i] = words[i].substr(0, 1).toUpperCase() + words[i].substr(1);
-  }
-  return words.join('');
-};
+})(typeof exports === 'undefined'? this['Beehive'] = {} : exports);
